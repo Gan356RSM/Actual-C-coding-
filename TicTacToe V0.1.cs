869e9b9ce51d0468492f75205c98efe4101bb33b
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2025-10-12 12:36:25 +0700

    Add files via upload

---
+++ TicTacToe V0.1.cs
@@ -0,0 +1,52 @@
+﻿string[,] tictoe = new string[3, 3] { 
+{ " 1 ", " 2 ", " 3 " }, 
+{ " 4 ", " 5 ", " 6 " },
+{ " 7 ", " 8 ", " 9 " } };
+string turn = " X ";
+bool win = false;
+
+for (int i = 0; i < tictoe.GetLength(0); i++) {
+        Console.WriteLine(tictoe[i, 0] + tictoe[i, 1] + tictoe[i, 2]);
+    }
+
+while (win == false) {
+
+    Console.WriteLine("Enter Coordinates X (0 --> 2):");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter Coordinates Y (0 --> 2):");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("-----------------------------------------------------");
+
+    bool moveMade = false;
+    while (moveMade == false) {
+        if (tictoe[x, y] == " X " || tictoe[x, y] == " O ") {
+            Console.WriteLine("Invalid Position");
+            Console.WriteLine("Enter Coordinates X (0 --> 2):");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Coordinates Y (0 --> 2):");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+        else 
+        {
+            tictoe[x, y] = turn;
+            moveMade = true;
+        }
+    }
+
+
+    for (int i = 0; i < tictoe.GetLength(0); i++) {
+        Console.WriteLine(tictoe[i, 0] + tictoe[i, 1] + tictoe[i, 2]);
+    }
+
+    Console.WriteLine("-----------------------------------------------------");
+    
+    if (turn == " O ") 
+    {
+        turn = " X "; 
+    }
+    else if (turn == " X ") 
+    {
+        turn = " O ";
+    }
+};
