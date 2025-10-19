a21380ecfe2e3db240bef973b99237b492d4ae21
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2025-10-19 12:30:13 +0700

    Update and rename TicTacToe V0.1.cs to TicTacToe V1.0.cs

--- TicTacToe V1.0.cs
+++ TicTacToe V1.0.cs
@@ -1,9 +1,28 @@
-﻿string[,] tictoe = new string[3, 3] { 
+string[,] tictoe = new string[3, 3] { 
 { " 1 ", " 2 ", " 3 " }, 
 { " 4 ", " 5 ", " 6 " },
 { " 7 ", " 8 ", " 9 " } };
 string turn = " X ";
 bool win = false;
+(int, int)[] a1 = {(0,0), (0,1), (0,2)};
+(int, int)[] a2 = {(1,0), (1,1), (1,2)};
+(int, int)[] a3 = {(2,0), (2,1), (2,2)};
+(int, int)[] a4 = {(0,0), (1,0), (2,0)};
+(int, int)[] a5 = {(0,1), (1,1), (2,1)};
+(int, int)[] a6 = {(0,2), (1,2), (2,2)};
+(int, int)[] a7 = {(0,0), (1,1), (2,2)};
+(int, int)[] a8 = {(0,2), (1,1), (2,0)};
+(int, int)[][] winCondition = { a1, a2, a3, a4, a5, a6, a7, a8 };
+
+void Check(string Thing) {
+    foreach ((int, int)[] won in winCondition) {
+        if (tictoe[won[0].Item1,won[0].Item2] == Thing && tictoe[won[1].Item1,won[1].Item2] == Thing && tictoe[won[2].Item1,won[2].Item2] == Thing) {
+            Console.WriteLine("You won");
+            win = true;
+        }
+    }
+}
+
 
 for (int i = 0; i < tictoe.GetLength(0); i++) {
         Console.WriteLine(tictoe[i, 0] + tictoe[i, 1] + tictoe[i, 2]);
@@ -49,4 +68,8 @@ while (win == false) {
     {
         turn = " O ";
     }
+
+    Check(" X ");
+    Check(" O ");
+    
 };
