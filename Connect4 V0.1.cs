f43d699b9d93da95fe516d577be90d08286e73a8
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2025-11-23 12:29:44 +0700

    Update Connect4 V0.2.cs

--- Connect4 V0.1.cs
+++ Connect4 V0.1.cs
@@ -1,4 +1,4 @@
-﻿string[,] connect67 = new string[6, 7] { 
+string[,] connect67 = new string[6, 7] { 
 { " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " }, 
 { " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " },
 { " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " },
@@ -18,18 +18,42 @@ while (win == false) {
 
     bool moveMade = false;
     while (moveMade == false) {
-        for (int i = 0; i < connect67.GetLength(0); i++) {
-            Console.WriteLine("run");
-            if (!(connect67[5 - i, y] == " 0 " || connect67[5 - i, y] == " 0 ")) {
-                connect67[5, y] = turn;
+        int bottom = 5;
+        while (bottom >= 0) {
+            if (connect67[bottom, y] == " _ ") {
+                connect67[bottom, y] = turn; 
                 moveMade = true;
+                break;
             }
+            bottom --;
         }
     }
 
     for (int i = 0; i < connect67.GetLength(0); i++) {
         Console.WriteLine(connect67[i, 0] + connect67[i, 1] + connect67[i, 2] + connect67[i, 3] + connect67[i, 4] + connect67[i, 5] + connect67[i, 6]);
     }
+
+    int loopCount = 0;
+    int count0 = 0;
+    int counto = 0;
+    
+
+    while (loopCount >= 6) {
+        if (connect67[loopCount, y] == " 0 ") {
+            count0 ++;
+            Console.WriteLine(" 0 UP ");
+            Console.WriteLine(count0);
+            Console.WriteLine(counto);
+        }
+        else if (connect67[loopCount, y] == " o ") {
+            counto ++;
+            Console.WriteLine(" o UP ");
+            Console.WriteLine(count0);
+            Console.WriteLine(counto);
+        }
+        loopCount ++;
+    }
+
 
     if (turn == " 0 ") 
     {
@@ -41,3 +65,4 @@ while (win == false) {
     }
 
 };
+
