b33a5dd67ddd22d00301db00a59fbab5b3e521c7
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2025-12-21 11:36:25 +0700

    Update Connect4 V1.0.cs

--- Connect4 V0.3.cs
+++ Connect4 V0.3.cs
@@ -2,9 +2,9 @@ string[,] connect67 = new string[6, 7] {
 { " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " }, 
 { " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " },
 { " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " },
-{ " _ ", " _ ", " _ ", " _ ", " _ ", " o ", " o " },
-{ " _ ", " _ ", " _ ", " _ ", " o ", " o ", " o " },
-{ " _ ", " _ ", " _ ", " o ", " o ", " o ", " 0 " }, };
+{ " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " },
+{ " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " },
+{ " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ " }, };
 
 string turn = " o ";
 bool win = false;   
@@ -53,155 +53,160 @@ while (win == false) {
         Console.WriteLine(connect67[i, 0] + connect67[i, 1] + connect67[i, 2] + connect67[i, 3] + connect67[i, 4] + connect67[i, 5] + connect67[i, 6]);
     }
 
-    int loopCount = 0;
-    int count0 = 0;
-    int counto = 0;
-    
 
-    while (loopCount < 6) {
-        if (connect67[loopCount, y] == " 0 ") {
-            count0 ++;
-            counto = 0;
-            if (count0 == 4) {
-                Console.WriteLine("0 win!");
-                win = true;
+    //checking rows
+    {
+        int loopCount = 0;
+        int count0 = 0;
+        int counto = 0;
+        
+
+        while (loopCount < 6) {
+            if (connect67[loopCount, y] == " 0 ") {
+                count0 ++;
+                counto = 0;
+                if (count0 == 4) {
+                    Console.WriteLine("0 win!");
+                    win = true;
+                }
             }
-        }
-        else if (connect67[loopCount, y] == " o ") {
-            counto ++;
-            count0 = 0;
-            if (counto == 4) {
-                Console.WriteLine("o win!");
-                win = true;
+            else if (connect67[loopCount, y] == " o ") {
+                counto ++;
+                count0 = 0;
+                if (counto == 4) {
+                    Console.WriteLine("o win!");
+                    win = true;
+                }
             }
+            loopCount ++; 
         }
-        loopCount ++; 
     }
 
-    count0 = 0;
-    counto = 0;
-    loopCount = 0;
 
-    while (loopCount < 7) {
-        if (connect67[bottom, loopCount] == " 0 ") {
-            count0 ++;
-            counto = 0;
-            if (count0 == 4) {
-                Console.WriteLine("0 win!");
-                win = true;
+    //checking columns
+    {
+        int count0 = 0;
+        int counto = 0;
+        int loopCount = 0;
+
+        while (loopCount < 7) {
+            if (connect67[bottom, loopCount] == " 0 ") {
+                count0 ++;
+                counto = 0;
+                if (count0 == 4) {
+                    Console.WriteLine("0 win!");
+                    win = true;
+                }
             }
-        }
-        else if (connect67[bottom, loopCount] == " o ") {
-            counto ++;
-            count0 = 0;
-            if (counto == 4) {
-                Console.WriteLine("o win!");
-                win = true;
+            else if (connect67[bottom, loopCount] == " o ") {
+                counto ++;
+                count0 = 0;
+                if (counto == 4) {
+                    Console.WriteLine("o win!");
+                    win = true;
+                }
             }
+            loopCount ++; 
         }
-        loopCount ++; 
     }
 
-    int curRow = bottom;
-    int curCol = y;
+    {   
+        int count0 = 0;
+        int counto = 0;
+        int loopCount = 0;
+        int curRow = bottom;
+        int curCol = y;
 
-    while (true) {
-        if (curRow == 0 || curCol == 0) {
-            break;
-        }
-        curRow --;
-        curCol --;
-    }
-
-    int startRow = curRow;
-    int startCol = curCol;
-
-    while (true) {
-        if (curRow == 5 || curCol == 6) {
-            break;
-        }
-        curRow ++;
-        curCol ++;
-    }
-
-    int endRow = curRow;
-    int endCol = curCol;
-
-    count0 = 0;
-    counto = 0;
-    loopCount = 0;
-
-    while (loopCount < (endCol - startCol) + 1) {
-        if (connect67[startRow + loopCount, startCol + loopCount] == " 0 ") {
-            count0 ++;
-            counto = 0;
-            if (count0 == 4) {
-                Console.WriteLine("0 win!");
-                win = true;
+        while (true) {
+            if (curRow == 0 || curCol == 0) {
+                break;
             }
+            curRow --;
+            curCol --;
         }
-        else if (connect67[startRow + loopCount, startCol + loopCount] == " o ") {
-            counto ++;
-            count0 = 0;
-            if (counto == 4) {
-                Console.WriteLine("o win!");
-                win = true;
+
+        int startRow = curRow;
+        int startCol = curCol;
+
+        while (true) {
+            if (curRow == 5 || curCol == 6) {
+                break;
             }
+            curRow ++;
+            curCol ++;
         }
-        loopCount ++; 
+
+        int endRow = curRow;
+        int endCol = curCol;
+
+        while (loopCount < (endCol - startCol) + 1) {
+            if (connect67[startRow + loopCount, startCol + loopCount] == " 0 ") {
+                count0 ++;
+                counto = 0;
+                if (count0 == 4) {
+                    Console.WriteLine("0 win!");
+                    win = true;
+                }
+            }
+            else if (connect67[startRow + loopCount, startCol + loopCount] == " o ") {
+                counto ++;
+                count0 = 0;
+                if (counto == 4) {
+                    Console.WriteLine("o win!");
+                    win = true;
+                }
+            }
+            loopCount ++; 
+        }
     }
 
-    curRow = bottom;
-    curCol = y;
+    {   
+        int count0 = 0;
+        int counto = 0;
+        int loopCount = 0;
+        int curRow = bottom;
+        int curCol = y;
 
-    while (true) {
-        if (curRow == 0 || curCol == 6) {
-            break;
-        }
-        curRow --;
-        curCol ++;
-    }
-
-    startRow = curRow;
-    startCol = curCol;
-
-    while (true) {
-        if (curRow == 5 || curCol == 0) {
-            break;
-        }
-        curRow ++;
-        curCol --;
-    }
-
-    endRow = curRow;
-    endCol = curCol;
-
-    Console.WriteLine(startCol - endCol);
-
-    while (loopCount < (startCol - endCol) + 1) {
-        Console.WriteLine(connect67[startRow + loopCount, endCol + loopCount]);
-        Console.WriteLine(startRow + loopCount);
-        Console.WriteLine(endCol + loopCount);
-        if (connect67[startRow + loopCount, endCol + loopCount] == " 0 ") {
-            count0 ++;
-            counto = 0;
-            Console.WriteLine(count0);
-            Console.WriteLine(counto);
-            if (count0 == 4) {
-                Console.WriteLine("0 win!");
-                win = true;
+        while (true) {
+            if (curRow == 0 || curCol == 6) {
+                break;
             }
+            curRow --;
+            curCol ++;
         }
-        else if (connect67[startRow + loopCount, endCol + loopCount] == " o ") {
-            counto ++;
-            count0 = 0;
-            Console.WriteLine(count0);
-            Console.WriteLine(counto);
-            if (counto == 4) {
-                Console.WriteLine("o win!");
-                win = true;
+
+        int startRow = curRow;
+        int startCol = curCol;
+
+        while (true) {
+            if (curRow == 5 || curCol == 0) {
+                break;
             }
+            curRow ++;
+            curCol --;
         }
-        loopCount ++; 
+
+        int endRow = curRow;
+        int endCol = curCol;
+
+        while (loopCount < (startCol - endCol) + 1) {
+            if (connect67[startRow + loopCount, startCol - loopCount] == " 0 ") {
+                count0 ++;
+                counto = 0;
+                if (count0 == 4) {
+                    Console.WriteLine("0 win!");
+                    win = true;
+                }
+            }
+            else if (connect67[startRow + loopCount, startCol - loopCount] == " o ") {
+                counto ++;
+                count0 = 0;
+                if (counto == 4) {
+                    Console.WriteLine("o win!");
+                    win = true;
+                }
+            }
+            loopCount ++; 
+        }
     }
 }
